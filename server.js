e5f570a340995ed52d4403ac390f049785989c23
hrana564 <hrana564@gmail.com>
2017-12-19 22:36:36 +0530

    Cross origin issue solved

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({secret : config.secretKey,resave:false,saveUninitialized:true,cookie:{maxAge:1000*60*60}}));
+// use it before all route definitions
+app.use(cors({origin: '*'}));
 app.use('/',defaultRouter);
 app.use('/login',defaultRouter);
 app.use('/signup',signupRouter);
